e5409a2ef954afb73d454589550e22b18601ef20
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2022-03-08 11:03:11 -0500

    Update restaurant.py

--- chapter9/restaurant.py
+++ chapter9/restaurant.py
@@ -10,7 +10,30 @@ class Restaurant():
     def open_restaurant(self):
         print("The restaurant " + self.restaurant_name.title() + " is OPEN!")
 
+class User():
+
+    def __init__(self, first_name, last_name, age, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.gender = gender
+
+    # method
+    def describe_user(self):
+        print("User's name is " + self.first_name.title() + " " + self.last_name.title() + ".")
+        print("User's age is " + str(self.age) + ".")
+        print("User's gender is " + self.gender + ".")
+
+    def greet_user(self):
+        print("Hello " + self.first_name.title() + "!")
+
+# instances
 my_restaurant = Restaurant('Pizzaria', "Hoggie")
 
 my_restaurant.descritbe_restaurant()
 my_restaurant.open_restaurant()
+
+my_user = User("Ryan", "Vola", 15, "Male")
+
+my_user.describe_user()
+my_user.greet_user()
